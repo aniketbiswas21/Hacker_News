b27de6bcb413136f6b265cd0bb4f182e265e92fa
aniketbiswas21 <aniket.biswas75@gmail.com>
2020-04-18 16:30:48 +0530

    Bugs Fixed

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
         <div className="navbar-fixed">
            <nav>
         <div class="nav-wrapper #1a237e indigo darken-4">
-          <a href="" className="brand-logo" ><span><i className="fab fa-hacker-news-square fa-lg"/>Hacker News</span></a>
+          <span className='brand-logo'><i className="fab fa-hacker-news-square fa-lg"/>Hacker News</span>
           <ul id="nav-mobile" class="right hide-on-med-and-down">
             <li><Link to='/'>Home</Link></li>
             <li><Link to='/top'>Top</Link></li>
